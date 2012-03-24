faffdaf283d0d37ab4a00cb26e4fe2f9b82fe635
Rickard Andersson <rickard@montania.se>
2012-03-24 17:16:19 +0100

    improved code

--- base64-image.php
+++ base64-image.php
@@ -4,30 +4,38 @@ Plugin Name: Base 64 Image
 Plugin URI: http://github.com/rickard2/WP-Base64-Image
 Description: Base 64 Image
 Author: Rickard Andersson
-Version: 0.00000000000001 dunderalpha
+Version: 0.00000000000001 alpha
 Author URI: https://0x539.se/
 */
 
 function b64img_insert_post_data( $post ) {
 
-	preg_match_all('/ src=\\\"(http[^\'"]+)\\\"/', $post['post_content'], $matches);
+	$count = preg_match_all('/ src=\\\"(http[^"]+)\\\"/', $post['post_content'], $matches);
 
-	foreach ( $matches[1] as $image ) {
+	if ( !$count || $count == 0 ) {
+		return $post;
+	}
+
+	foreach ( $matches[1] as $key => $image ) {
 
 		switch ( strtolower( substr( $image, -3, 3) ) ) {
 			case 'jpg': $mime = 'image/jpeg'; break;
 			case 'gif': $mime = 'image/gif'; break;
 			case 'png': $mime = 'image/png'; break;
+			default: $mime = false;
 		}
 
-		$new_image = 'data:' . $mime . ';base64,' . base64_encode( file_get_contents($image) ) . '\" data-image-url=\"' . $image;
+		if ( $mime !== false ) {
 
-		$post['post_content'] = str_replace( $image, $new_image, $post['post_content'] );
+			$new_image = ' src="data:' . $mime . ';base64,' . base64_encode( file_get_contents($image) ) . '\" data-image-url=\"' . $image . '\"';
 
-		unset($mime, $new_image);
+			$post['post_content'] = str_replace( $matches[0][ $key ], $new_image, $post['post_content'] );
+
+			unset($new_image);
+		}
 	}
 		
 	return $post;
 }
 
-add_filter( 'wp_insert_post_data', 'b64img_insert_post_data' );
+add_filter( 'wp_insert_post_data', 'b64img_insert_post_data', 99999 );
